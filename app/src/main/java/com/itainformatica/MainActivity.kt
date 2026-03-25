f3f2cf9b3e924d8471fddc0d8f123e91b06dd791
Lucas <lucas.rasoppi@4innovation.com.br>
2026-03-25 17:01:06 -0300

    initialize separator component implementation

--- app/src/main/java/com/itainformatica/MainActivity.kt
+++ app/src/main/java/com/itainformatica/MainActivity.kt
@@ -14,10 +14,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.itainformatica.components.*
 import com.itainformatica.components.cards.CategoryCard
 import com.itainformatica.components.fields.SearchField
-import com.itainformatica.components.flags.CategoryFlag
 import com.itainformatica.components.pages.MainPage
 import com.itainformatica.ui.theme.AppitainformaticaTheme
 import com.itainformatica.ui.theme.DarkBlue
--- app/src/main/java/com/itainformatica/components/flags/SeparatorFlag.kt
+++ app/src/main/java/com/itainformatica/components/flags/SeparatorFlag.kt
@@ -1,2 +1,50 @@
 package com.itainformatica.components.flags
 
+import android.widget.TabWidget
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SeparatorFlag(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    color: Color = Color.Gray,
+    textColor: Color = Color.Black,
+    fontSize: Float = 12f,
+    fontWeight: FontWeight = FontWeight.Medium,
+    lineWidth: Float = 100f,
+    spacing: Dp = 5.dp
+){
+    Column (
+    ){
+        Text(
+            text = title,
+            color = textColor,
+            fontSize = fontSize.sp,
+            fontWeight = fontWeight
+        )
+        Spacer(modifier = Modifier.height(spacing))
+        Box(
+            modifier = Modifier
+                .background(color = color)
+                .width(lineWidth.dp)
+                .height(5.dp)
+        )
+    }
+}
