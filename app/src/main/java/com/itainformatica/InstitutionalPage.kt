1fe623b9e51aa808c49d8a1bef2ef80dbb20481f
gothlul <lrasoppi11@gmail.com>
2026-03-27 10:12:07 -0300

    commit test

--- app/src/main/java/com/itainformatica/InstitutionalPage.kt
+++ app/src/main/java/com/itainformatica/InstitutionalPage.kt
@@ -1,4 +1,25 @@
 package com.itainformatica
 
-class InstitutionalPage {
+import MainPage
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import com.itainformatica.ui.theme.AppitainformaticaTheme
+
+class InstitutionalPage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppitainformaticaTheme {
+                MainPage(
+                ){
+                    Column() {
+                    }
+                }
+            }
+        }
+    }
 }
