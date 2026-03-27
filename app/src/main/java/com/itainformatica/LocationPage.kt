eb1ee7dc9aa7f484084a6d76c9e71bcb6f286955
JuniorLegalPoggers <juniorti464@gmail.com>
2026-03-27 14:56:56 -0300

    mapa

--- app/src/main/java/com/itainformatica/LocationPage.kt
+++ app/src/main/java/com/itainformatica/LocationPage.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -17,6 +18,12 @@ import androidx.compose.ui.unit.dp
 import com.itainformatica.components.cards.BottomSheet
 import com.itainformatica.components.pages.MainPage
 import com.itainformatica.ui.theme.AppitainformaticaTheme
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.material3.Button
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 
 class LocationPage : ComponentActivity() {
 
@@ -30,13 +37,43 @@ class LocationPage : ComponentActivity() {
 
                         Text("Conteúdo que fica atrás do Sheet", modifier = Modifier.fillMaxSize())
 
-                        BottomSheet(
-                            modifier = Modifier.align(Alignment.BottomCenter),
-                            bottomLimit = 40f,
-                            topLimit = 80f
+                        val context = LocalContext.current
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
                         ) {
-                            Text("Nossas Lojas")
+
+                            Image(
+                                painter = painterResource(id = R.drawable.lojas),
+                                contentDescription = "lojas",
+                            )
+
+
+                            Button(
+                                onClick = {
+                                    val gmmIntentUri =
+                                        Uri.parse("geo:-22.9971829,-46.8704619?q=Ita Informática")
+                                    val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                                    mapIntent.setPackage("com.google.android.apps.maps")
+                                    context.startActivity(mapIntent)
+                                }
+                            ) {
+                                Text("Abrir no Maps")
+                            }
+
                         }
+
+//                        BottomSheet(
+//                            modifier = Modifier.align(Alignment.BottomCenter),
+//                            bottomLimit = 40f,
+//                            topLimit = 80f
+//                        ) {
+//                            Text("Nossas Lojas")
+//                        }
                     }
                 }
             }
